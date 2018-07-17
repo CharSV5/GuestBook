cdb1cd4a9b88ec12fbcaa3221fdb5f6ffd5fb08c
CharSV5 <charlenebastians@hotmail.com>
2018-07-17 15:17:56 +0100

    basic set up for GuestBook.js

---
+++ client/src/GuestBook.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class GuestBook extends Component {
+  render() {
+    return(
+      <div className="GuestBook">
+
+      My guestbook!
+      </div>
+    )
+  }
+}
+
+export default GuestBook;
