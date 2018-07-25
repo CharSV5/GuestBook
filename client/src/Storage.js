0fce6774bd1dce65452b0fbf8dfa90f43d73b7b7
CharSV5 <charlenebastians@hotmail.com>
2018-07-25 15:08:41 +0100

    refactor

--- client/src/Storage.js
+++ client/src/Storage.js
@@ -4,13 +4,20 @@ export class Storage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+    bin: [],
     comments: [] };
     this.addToStorage = this.addToStorage.bind(this);
   }
 
   addToStorage(propToAdd) {
-    this.state.comments.push(propToAdd)
+    this.state.bin.push(propToAdd)
+    console.log('look here' + this.state.bin.slice(-1)[0]);
+    this.state.comments.push(this.state.bin.slice(-1)[0])
     console.log(this.state);
+  }
+
+  addToStorage(propToAdd) {
+    
   }
 
   render() {
